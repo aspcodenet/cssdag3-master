4f541f990a847ab6a6013effb9e03628497d781c
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-02-24 13:42:14 +0100

    v1 meny

---
+++ script.js
@@ -0,0 +1,29 @@
+const hamburgerMenu = document.getElementById('hamburgermenu')
+const menuClose = document.getElementById('menuclose')
+const navMenu = document.querySelector('.navmenu');
+
+hamburgerMenu.addEventListener("click",  () => {
+    hamburgerMenu.style.display = 'none';
+    menuClose.style.display = 'inline-block';
+    navMenu.classList.toggle('active');
+});
+
+
+menuClose.addEventListener("click",  () => {
+    menuClose.style.display = 'none';
+    hamburgerMenu.style.display = 'inline-block';
+    navMenu.classList.toggle('active');
+});
+
+
+
+
+
+window.onscroll = () =>{
+    hamburgerMenu.style.display = null;
+    menuClose.style.display = null;
+    navMenu.classList.remove("active");    
+}
+
+
+
